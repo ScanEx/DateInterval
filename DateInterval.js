cde350ffa5cf761473fcb6c442ab1a72c9fae1fe
Alexander Parshin <parshin.alexander@gmail.com>
2015-12-18 19:08:44 +0300

    DateInterval: default values and some static methods

--- DateInterval.js
+++ DateInterval.js
@@ -1,7 +1,8 @@
 nsGmx.DateInterval = Backbone.Model.extend({
-    defaults: {
-        dateBegin: null,
-        dateEnd: null
+    initialize: function() {
+        if (!('dateBegin' in this.attributes) && !('dateEnd' in this.attributes)) {
+            this.set(nsGmx.DateInterval.getUTCDayBoundary());
+        }
     },
     
     saveState: function() {
@@ -17,5 +18,23 @@ nsGmx.DateInterval = Backbone.Model.extend({
             dateBegin: new Date(state.dateBegin),
             dateEnd: new Date(state.dateEnd)
         })
+    }
+}, {
+    //number of milliseconds in one day
+    MS_IN_DAY: 24*3600*1000,
+    
+    //set time to UTC midnight
+    toMidnight: function(date) {
+        return new Date(date - date % nsGmx.DateInterval.MS_IN_DAY);
+    },
+    
+    getUTCDayBoundary: function(date) {
+        date = date || new Date();
+        
+        var midnight = nsGmx.DateInterval.toMidnight(date);
+        return {
+            dateBegin: midnight,
+            dateEnd: new Date(midnight.valueOf() + nsGmx.DateInterval.MS_IN_DAY)
+        }
     }
 });

dd66d5d6fd6b3881cd4468dcc2603692e88fe6eb
zverev <alexander.zverev@gmail.com>
2016-10-21 14:29:48 +0300

    commonjs support

--- DateInterval.js
+++ DateInterval.js
@@ -1,44 +1,55 @@
-nsGmx.DateInterval = Backbone.Model.extend({
-    initialize: function() {
-        if (!('dateBegin' in this.attributes) && !('dateEnd' in this.attributes)) {
-            this.set(nsGmx.DateInterval.getUTCDayBoundary());
-        }
-    },
-    
-    saveState: function() {
-        return {
-            version: '1.1.0',
-            dateBegin: +this.attributes.dateBegin,
-            dateEnd: +this.attributes.dateEnd
-        }
-    },
-    
-    loadState: function(state) {
-        if (!state.version || state.version === '1.1.0' || state.version === '1.0.0') {
-            this.set({
-                dateBegin: new Date(state.dateBegin),
-                dateEnd: new Date(state.dateEnd)
-            })
-        } else {
-            throw 'Unknown state version';
-        }
+(function (factory) {
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory()
+    } else {
+        window.nsGmx = window.nsGmx || {}
+        window.nsGmx.DateInterval = factory()
     }
-}, {
-    //number of milliseconds in one day
-    MS_IN_DAY: 24*3600*1000,
-    
-    //set time to UTC midnight
-    toMidnight: function(date) {
-        return new Date(date - date % nsGmx.DateInterval.MS_IN_DAY);
-    },
-    
-    getUTCDayBoundary: function(date) {
-        date = date || new Date();
-        
-        var midnight = nsGmx.DateInterval.toMidnight(date);
-        return {
-            dateBegin: midnight,
-            dateEnd: new Date(midnight.valueOf() + nsGmx.DateInterval.MS_IN_DAY)
+})(function() {
+    var DateInterval = window.Backbone.Model.extend({
+        initialize: function() {
+            if (!('dateBegin' in this.attributes) && !('dateEnd' in this.attributes)) {
+                this.set(DateInterval.getUTCDayBoundary());
+            }
+        },
+
+        saveState: function() {
+            return {
+                version: '1.1.0',
+                dateBegin: +this.attributes.dateBegin,
+                dateEnd: +this.attributes.dateEnd
+            }
+        },
+
+        loadState: function(state) {
+            if (!state.version || state.version === '1.1.0' || state.version === '1.0.0') {
+                this.set({
+                    dateBegin: new Date(state.dateBegin),
+                    dateEnd: new Date(state.dateEnd)
+                })
+            } else {
+                throw 'Unknown state version';
+            }
         }
-    }
-});
+    }, {
+        //number of milliseconds in one day
+        MS_IN_DAY: 24*3600*1000,
+
+        //set time to UTC midnight
+        toMidnight: function(date) {
+            return new Date(date - date % DateInterval.MS_IN_DAY);
+        },
+
+        getUTCDayBoundary: function(date) {
+            date = date || new Date();
+
+            var midnight = DateInterval.toMidnight(date);
+            return {
+                dateBegin: midnight,
+                dateEnd: new Date(midnight.valueOf() + DateInterval.MS_IN_DAY)
+            }
+        }
+    })
+
+    return DateInterval
+})
